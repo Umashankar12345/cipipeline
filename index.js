d969a5d6822882ba4bba3b25d0b259069d55e61c
Umashankar Kumar <umashankarkumar9572@gmail.com>
2026-05-06 16:03:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+const PORT= 3000
+
+app.get('/' , (req , res) => {
+    res.send('Hello World from the user')
+})
+console.log("Server is running  on Port http://localhost:" + PORT)
+
+app.listen(PORT , () => {
+    console.log("Server is running  on Port http://localhost:" + PORT)
+})
