0577e1f94c0f6477dc58dde92ce7df71220ad6c3
Umashankar kumar <Umashankarkumar9572@gmail.com>
2026-05-11 23:52:11 +0530

    Fix console log message for server port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT= 3000
 app.get('/' , (req , res) => {
     res.send('Hello World from  umashankar')
 })
-console.log("Server is running  on Port http://localhost:" + PORT)
+console.log("Server is running  on this  Port http://localhost:" + PORT)
 
 app.listen(PORT , () => {
     console.log("Server is running  on Port http://localhost:" + PORT)
